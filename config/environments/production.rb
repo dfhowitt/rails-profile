645cd87b6ebc5193b9580744c720a6730315edd2
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-06-29 17:07:21 +0100

    pushing to deployment II

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'new_app_name.herokuapp.com',
+    domain:               'rails-dhowitt.herokuapp.com',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
